9074d920cab4977b2acfc19fb877496f20fab30c
ShubhankarKG <shubhankar.gupto.11@gmail.com>
2020-04-19 11:50:24 +0530

    New UI
    The client folder is refactored now. This UI now supports Material UI.
    
    Changes and future work :
    1. Material UI added
    2. Redux not implemented.

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,12 @@
 import React from 'react';
+import Auth from "./components/Auth";
 import './App.css';
-
-import HomePage from './components/HomePage';
-import Profile from "./components/Profile";
-import { Route, Switch } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from "./store";
 
 function App() {
   return (
-    <Provider store={store}>
-      <Switch>
-        <Route exact path="/profile" component={Profile} />
-      </Switch>
-      <HomePage />
-    </Provider>
+    <div className="App">
+      <Auth/>
+    </div>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-    <BrowserRouter basename="/api"> 
-        <App />
-    </BrowserRouter>
-    , document.getElementById('root'));
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
