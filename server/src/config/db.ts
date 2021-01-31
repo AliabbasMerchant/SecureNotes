15dd36662cec134268448ea97e02b362b6792b5b
ShubhankarKG <shubhankar.gupto.11@gmail.com>
2021-01-31 16:05:35 +0530

    add logger, session, fix tsconfig

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,7 +1,4 @@
-import dotenv from "dotenv";
 import mongoose from "mongoose";
-
-dotenv.config();
 
 function dbConnect() {
   const mongooseOptions: mongoose.ConnectionOptions = {
--- server/src/router.ts
+++ server/src/router.ts
@@ -6,10 +6,4 @@ router.get("/", (req, res) => {
   res.status(200).json({ message: "YO" });
 });
 
-if (process.env.NODE_ENV === "production") {
-  router.get("*", (req, res) => {
-    res.sendFile(__dirname + "/client/build/index.html");
-  });
-}
-
 export default router;
